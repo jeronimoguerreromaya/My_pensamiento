0fcb3741434c33c1dba253f753397a2cc2156172
jero <omineroj@gmail.com>
2025-11-28 19:22:49 -0500

    my_pensameinto initial

---
+++ src/main/java/com/mypensamiento/mypensamiento/MyPensamientoApplication.java
@@ -0,0 +1,13 @@
+package com.mypensamiento.mypensamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPensamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPensamientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mypensamiento/mypensamiento/MyPensamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypensamiento.mypensamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPensamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
